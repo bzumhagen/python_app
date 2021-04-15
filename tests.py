9d0189746d806cbb30dd41dc0931f2cd26bd5dac
Ben Zumhagen <ben.zumhagen@relativity.com>
2021-04-14 20:56:18 -0500

    Initial commit

---
+++ tests.py
@@ -0,0 +1,6 @@
+from hello import app
+with app.test_client() as client:
+    response = client.get('/')
+    assert response.data == b'Hello World!'
+    assert response.status_code == 200
+    print("Tests Passed")
